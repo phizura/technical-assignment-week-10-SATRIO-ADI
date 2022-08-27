85190161740a0eb984687caf6fc9a43dee33d9c0
Satrio <107029886+phizura@users.noreply.github.com>
2022-08-27 13:39:27 +0700

    Add files via upload

---
+++ code.py
@@ -0,0 +1,71 @@
+import time
+import requests
+import os
+
+
+TOKEN = "BBFF-fOBpYuxMljGnFrL2x5FiyuUMA4ewon" 
+DEVICE_LABEL = "aquathings" 
+VARIABLE_LABEL_1 =  "suhu" 
+    
+def sensor():
+    for i in os.listdir('/sys/bus/w1/devices'):
+        if i != 'w1_bus_master1':
+            ds18b20 = i
+    return ds18b20
+
+def read(ds18b20):
+    location = '/sys/bus/w1/devices/' + ds18b20 + '/w1_slave'
+    tfile = open(location)
+    text = tfile.read()
+    tfile.close()
+    secondline = text.split("\n")[1]
+    temperaturedata = secondline.split(" ")[9]
+    temperature = float(temperaturedata[2:])
+    celsius = temperature / 1000
+    #farenheit = (celsius * 1.8) + 32
+    return celsius
+                  
+def build_payload(variable_1): 
+    value_1 = read("28-3c01f096ee94")
+    payload = {variable_1: value_1}
+    return payload
+
+
+def post_request(payload):
+    # Creates the headers for the HTTP requests
+    url = "http://industrial.api.ubidots.com"
+    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
+    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
+
+    # Makes the HTTP requests
+    status = 400
+    attempts = 0
+    while status >= 400 and attempts <= 5:
+        req = requests.post(url=url, headers=headers, json=payload)
+        status = req.status_code
+        attempts += 1
+        time.sleep(1)
+
+    # Processes results
+    print(req.status_code, req.json())
+    if status >= 400:
+        print("[ERROR] Could not send data after 5 attempts, please check \
+            your token credentials and internet connection")
+        return False
+
+    print("[INFO] request made properly, your device is updated")
+    return True
+
+
+def main():
+    payload = build_payload(VARIABLE_LABEL_1)
+
+    print("[INFO] Attemping to send data")
+    post_request(payload)
+    print("[INFO] finished")
+
+
+if __name__ == '__main__':
+    while (True):
+        main()
+        time.sleep(1)
